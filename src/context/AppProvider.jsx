bf7a59504eea8d016d22e1e1c1330a516371a9f4
baokhanh1701 <khanhnkb@gmail.com>
2024-03-23 10:32:21 +0700

    chore: setup project folder struct + context

---
+++ src/context/AppProvider.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+    getAllEvents,
+} from "../services/events";
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height,
+    };
+}
+function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(
+        getWindowDimensions()
+    );
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return windowDimensions;
+}
+
+export const AppContext = React.createContext("");
+
+export default function AppProvider({ children }) {
+    const navigate = useNavigate();
+    const { width, height } = useWindowDimensions();
+    const commonBreakPoint = [320, 480, 768, 1024, 1025, 1200];
+    const [events, setEvents] = useState([]);
+    const [initRenderFlag, setInitRenderFlag] = useState(true);
+
+    const upcomingEvents = events.filter((event) => {
+        const end_time = event.end_time;
+        return new Date().valueOf() < new Date(end_time).valueOf();
+    }).sort((a, b) => {
+        return new Date(a.start_time) - new Date(b.start_time)
+    });
+
+    const pastEvents = events.filter((event) => {
+        const end_time = event.end_time;
+        return new Date().valueOf() > new Date(end_time).valueOf();
+    }).sort((a, b) => {
+        return new Date(b.end_time) - new Date(a.end_time)
+    });
+
+    const fetchEventsData = async () => {
+        // console.log(localStorage.getItem("profile-dataaaa"));
+        const id = JSON.parse(localStorage.getItem("profile-data"))?._id;
+        if (id != null) {
+            const events_update = await getAllEvents({
+                user_id: id,
+            });
+            setEvents(events_update.data);
+        }
+        else console.log("Can't get Events Data since user id is not exist")
+    };
+
+    // useEffect(() => {
+    //   fetchEventsData()
+    // }, [])
+
+    return (
+        <AppContext.Provider
+            value={{
+                width,
+                height,
+                commonBreakPoint,
+                navigate,
+                events,
+                setEvents,
+                upcomingEvents,
+                pastEvents,
+                initRenderFlag,
+                setInitRenderFlag,
+                fetchEventsData
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    );
+}
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { auth } from "../services/firebase/config";
+import { useNavigate, useLocation } from "react-router-dom";
+export const AuthContext = React.createContext();
+
+const unAuthPage = [
+    "/",
+    "/signin",
+    "/signup",
+    "/event",
+    // "/user-info",
+];
+
+export default function AuthProvider({ children }) {
+    const [profileData, setProfileData] = useState({
+        _id: "",
+        name: "",
+        type: "",
+        email: "",
+        avatar: "",
+        bio: "You can edit this!",
+    });
+    const navigate = useNavigate();
+    const location = useLocation();
+    // const [user, setUser] = useState(null);
+    //   const [profileData, setProfileData] = useState(null);
+    useEffect(() => {
+        const unsubscribed = auth.onAuthStateChanged((userInfo) => {
+            if (userInfo || localStorage.getItem("profile-data")) {
+                if (unAuthPage.includes(location.pathname)) {
+                    navigate("/home");
+                }
+                return;
+            }
+            if (!unAuthPage.includes(location.pathname)) {
+                navigate("/");
+            }
+        });
+        return () => {
+            unsubscribed();
+        };
+    }, [navigate, location.pathname]);
+
+    useEffect(() => {
+        const profileData = JSON.parse(localStorage.getItem("profile-data"));
+        if (profileData) {
+            setProfileData(profileData);
+        }
+    }, []);
+    return (
+        <AuthContext.Provider
+            value={{
+                profileData,
+                setProfileData,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+}
