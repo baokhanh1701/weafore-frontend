e214d4e645a90c280e14ada9e804834a47d28654
baokhanh1701 <khanhnkb@gmail.com>
2024-03-23 10:43:54 +0700

    chore: setup landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
 import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
 // import Calendars from "./pages/Calendars";
 // import CreateEvent from "./pages/CreateEvent";
 // import EventsManager from "./pages/EventsManager";
-import Personal from "./pages/Personal";
+// import Personal from "./pages/Personal";
 // import Settings from "./pages/Settings";
-import UserProfile from "./pages/UserProfile";
+// import UserProfile from "./pages/UserProfile";
 // import DiscoverEvents from "./pages/DiscoverEvents";
 // import CalendarsManager from "./pages/CalendarsManager";
 // import CreateCalendar from "./pages/CreateCalendar";
 // import CalendarSettings from "./pages/CalendarSettings";
 // import EventSettings from "./pages/EventSettings";
-import ErrorPage from "./pages/ErrorPage";
+// import ErrorPage from "./pages/ErrorPage";
 // import AuthProvider from "./context/AuthProvider";
 // import AppProvider from "./context/AppProvider";
 
 const LandingPage = React.lazy(() => import("./pages/LandingPage"));
-const EventPage = React.lazy(() => import("./pages/EventPage"));
-const Home = React.lazy(() => import("./pages/Home"))
+// const EventPage = React.lazy(() => import("./pages/EventPage"));
+// const Home = React.lazy(() => import("./pages/Home"))
 
 function App() {
   const routes = [
-    {
-      path: "/*",
-      element: <ErrorPage />,
-    },
+    // {
+    //   path: "/*",
+    //   element: <ErrorPage />,
+    // },
     {
       path: "/",
       element: (
@@ -36,54 +36,50 @@ function App() {
         </Suspense>
       ),
     },
-    {
-      path: "/event/:eid",
-      element: <Suspense fallback={<div>Loading...</div>}>
-        <EventPage />
-      </Suspense>,
-    },
-    {
-      path: "/event/manage/:eid",
-      element: <EventPage />,
-    },
-    {
-      path: "/signin",
-      element: <SignIn />,
-    },
-    {
-      path: "/signup",
-      element: <SignUp />,
-    },
-    {
-      path: "/home",
-      element: <Suspense fallback={<div>Loading...</div>}>
-        <Home />
-      </Suspense>,
-    },
+    // {
+    //   path: "/event/:eid",
+    //   element: <Suspense fallback={<div>Loading...</div>}>
+    //     <EventPage />
+    //   </Suspense>,
+    // },
+    // {
+    //   path: "/event/manage/:eid",
+    //   element: <EventPage />,
+    // },
+    // {
+    //   path: "/signin",
+    //   element: <SignIn />,
+    // },
+    // {
+    //   path: "/signup",
+    //   element: <SignUp />,
+    // },
+    // {
+    //   path: "/home",
+    //   element: <Suspense fallback={<div>Loading...</div>}>
+    //     <Home />
+    //   </Suspense>,
+    // },
     // {
     //   path: "/home/calendars",
     //   element: <Calendars />,
     // },
-    {
-      path: "/personal",
-      element: <Personal />,
-    },
+    // {
+    //   path: "/personal",
+    //   element: <Personal />,
+    // },
     // {
     //   path: "/discover",
     //   element: <DiscoverEvents />,
     // },
-    {
-      path: "/personal",
-      element: <Personal />,
-    },
     // {
     //   path: "/discover",
     //   element: <DiscoverEvents />,
     // },
-    {
-      path: "/user/:id",
-      element: <UserProfile />,
-    },
+    // {
+    //   path: "/user/:id",
+    //   element: <UserProfile />,
+    // },
     // {
     //   path: "/user/:uid/settings",
     //   element: <Settings />,
