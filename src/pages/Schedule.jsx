36c3f20f53a1f4514e8c3f6d6ba5e161d03c314f
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-04-13 11:28:09 +0700

    feat: history of weather

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -4,7 +4,9 @@ import getCurrentWeatherApi from '../services/weatherapi/getCurrentWeatherApi';
 import WeatherCards from '../components/WeatherCards';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-
+import getWeatherHistory from '../services/weatherapi/getWeatherHistory';
+import jsonata from 'jsonata';
+import LineChart from '../components/LineChart';
 const { Content } = Layout;
 const { Text } = Typography;
 
@@ -14,9 +16,13 @@ const Schedule = () => {
     const [currentWeather, setCurrentWeather] = useState({})
     const [date, setDate] = useState(() => dayjs('2017-01-25'));
     const [selectedDate, setSelectedDate] = useState(() => dayjs('2017-01-25'))
-    const [history, setHistory] = useState({})
+    const [weatherHistory, setWeatherHistory] = useState({})
     const [forecast, setForecast] = useState({})
+    const [temperatureLine, setTemperatureLine] = useState({})
+    const [humidityLine, setHumidityLine] = useState({})
+
     const [weatherModalOpen, setWeatherModalOpen] = useState(false);
+    const [alertStatus, setAlertStatus] = useState('info')
 
     const {
         token: { colorBgContainer, borderRadiusLG },
@@ -24,40 +30,52 @@ const Schedule = () => {
     const onPanelChange = (value, mode) => {
         console.log(value.format('YYYY-MM-DD'), mode);
     };
-
     const fetchCurrentWeatherData = async () => {
         const result = await getCurrentWeatherApi();
         setCurrentWeather(result);
         return result;
     }
+    const temperature_line_jsonata = jsonata("$.forecast.forecastday.hour.{    'date': $substringAfter($.time, ' '),    'value': $.temp_c}")
+    const humidity_line_jsonata = jsonata("$.forecast.forecastday.hour.{    'date': $substringAfter($.time, ' '),    'value': $.humidity}")
 
     const openWeatherModal = async (newDate) => {
         const now = dayjs()
-        console.log(now)
-        // console.log(now.to(newDate))
         const nowCondition = now.to(newDate)
-        // console.log(nowCondition[3])
-        if (
-            nowCondition == 'a day ago'
+        const historyCondition = (
+            nowCondition[0] == 'a'
             ||
-            parseInt(nowCondition[0]) <= 5
-        ) {
-            // console.log("condition 1")
-            setWeatherModalOpen(true)
-            setDate(newDate);
-            setSelectedDate(newDate);
-        } else if (
+            parseInt(nowCondition.substring(0, nowCondition.indexOf(' ', 0))) <= 5
+        )
+        const futureCondition = (
             nowCondition == 'in a day'
             ||
-            parseInt(nowCondition[3]) <= 5
-        ) {
+            parseInt(nowCondition.substring(3, nowCondition.indexOf(' ', 3))) <= 5
+        )
 
+        // console.log(now)
+        // console.log(now.to(newDate))
+        console.log(nowCondition)
+        if (historyCondition) {
+            // console.log("condition 1")
+            const weatherHistoryAtDate = await getWeatherHistory(newDate)
+            const _temperatureLine = await temperature_line_jsonata.evaluate(weatherHistoryAtDate)
+            const _humidityLine = await humidity_line_jsonata.evaluate(weatherHistoryAtDate)
+            setDate(newDate);
+            setSelectedDate(newDate);
+            setAlertStatus('success')
+            setWeatherModalOpen(true)
+            // console.log(_temperatureLine)
+            setWeatherHistory(weatherHistoryAtDate)
+            setTemperatureLine(_temperatureLine)
+            setHumidityLine(_humidityLine)
+        } else if (futureCondition) {
             // console.log("condition 2")
+            setAlertStatus('success')
             setWeatherModalOpen(true)
             setDate(newDate);
             setSelectedDate(newDate);
         } else {
-            console.log("Out of bound")
+            setAlertStatus('error')
         }
     }
 
@@ -121,8 +139,22 @@ const Schedule = () => {
                         </Text>
                         <WeatherCards currentWeather={currentWeather ? currentWeather : "Loading"} />
                         <Alert
-                            type={""}
-                            message={`You selected date: ${selectedDate?.format('YYYY-MM-DD')}`}
+                            showIcon
+                            // message={
+                            //     (alertStatus == 'success')
+                            //     ?
+                            //     "Success"
+                            //     :
+                            //     "Limited"
+                            // }
+                            type={alertStatus}
+                            message={
+                                (alertStatus == 'success')
+                                    ?
+                                    `You selected date: ${selectedDate?.format('YYYY-MM-DD')}`
+                                    :
+                                    "Our services are only able to track weather history of forecast up to 6 days."
+                            }
                         />
                         <Modal
                             title="Weather Report"
@@ -130,10 +162,48 @@ const Schedule = () => {
                             open={weatherModalOpen}
                             onOk={() => setWeatherModalOpen(false)}
                             onCancel={() => setWeatherModalOpen(false)}
+                            width={1000}
+
                         >
-                            <p>some contents...</p>
-                            <p>some contents...</p>
-                            <p>some contents...</p>
+                            <Text
+                                style={{
+                                    fontSize: "1rem",
+                                }}
+                                strong
+                            >
+                                Overall
+                            </Text>
+                            <p>Average Humidity: {weatherHistory?.forecast?.forecastday[0]?.day?.avghumidity} (%)</p>
+                            <p>Average Temperature: {weatherHistory?.forecast?.forecastday[0]?.day?.avgtemp_c} (°C)</p>
+                            <p>Mininum Temperature: {weatherHistory?.forecast?.forecastday[0]?.day?.mintemp_c} (°C)</p>
+                            <p>Maximum Temperature: {weatherHistory?.forecast?.forecastday[0]?.day?.maxtemp_c} (°C)</p>
+                            <p>Total Precipitation: {weatherHistory?.forecast?.forecastday[0]?.day?.totalprecip_mm} (mm) </p>
+                            <p>Chance of rain: {weatherHistory?.forecast?.forecastday[0]?.day?.daily_will_it_rain} (%) </p>
+                            <br />
+
+                            <Text
+                                style={{
+                                    fontSize: "1rem",
+                                }}
+                                strong
+                            >
+                                Temperature (Per Hours)
+                            </Text>
+                            <br />
+                            <LineChart data={temperatureLine} />
+
+                            <br />
+
+                            <Text
+                                style={{
+                                    fontSize: "1rem",
+                                }}
+                                strong
+                            >
+                                Humidity (Per Hours)
+                            </Text>
+                            <br />
+                            <LineChart data={humidityLine} />
                         </Modal>
                         <Calendar
                             onSelect={openWeatherModal}
